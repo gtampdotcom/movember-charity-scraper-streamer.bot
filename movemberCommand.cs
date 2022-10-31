23c08f6bdbd8575c1d5bf443dfb3c3729805320e
GTAMP <gtamulti@gmail.com>
2022-10-31 11:14:31 +1100

    Create movemberCommand.cs

---
+++ movemberCommand.cs
@@ -0,0 +1,25 @@
+// https://github.com/gtampdotcom/movember-charity-scraper-streamer.bot
+
+using System;
+using System.IO;
+		
+public class CPHInline
+{
+	public bool Execute()
+	{
+		string total = "0";
+		string searchWord = "<h1 class=\"mospace-heroarea--donations-target-amount-number\">";
+		string searchWordEnd = "</h1>";
+
+		var page = args["page"].ToString();
+		var filename = args["filename"].ToString();
+		int start = page.IndexOf(searchWord)+searchWord.Length+1;
+		int end = page.IndexOf(searchWordEnd,start);
+		total = page.Substring(start,end-start).Trim();
+		
+		CPH.SendMessage("Total: " + total);
+		File.WriteAllText(filename, total);
+
+		return true;
+	}
+}
